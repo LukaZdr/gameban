8e84e997c79d6096eaa0d9154aa6275d1a3f17a5
Luka Zdravkovic <luka.z@hotmail.de>
2019-11-26 23:34:57 +0100

    set name to user when created and display that name in navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  before_create :give_anonyme_name
+
+  private
+  def give_anonyme_name
+    self.first_name = 'Gustav'
+    self.last_name = 'Horst'
+  end
 end
