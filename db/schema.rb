866d4aa290bc2b6a45836fa498d922ce1676eb88
Luka Zdravkovic <luka.z@hotmail.de>
2019-11-26 23:16:50 +0100

    make devise work

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_26_215132) do
+ActiveRecord::Schema.define(version: 2019_11_26_220904) do
 
   create_table "projects", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 2019_11_26_215132) do
     t.bigint "level"
     t.bigint "xp"
     t.integer "picture_id"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
