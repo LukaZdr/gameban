9e4a12f2aa99147fd4743fde24a95c9d72e8c335
Luka Zdravkovic <luka.z@hotmail.de>
2019-11-30 23:29:43 +0100

    display dependencys in scoreboard

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,15 +6,35 @@ class User < ApplicationRecord
 
   before_validation :give_anonyme_name
 
+  def gain_xp(gained_xp)
+    prior_xp = xp
+    self.xp += gained_xp
+    save!
+    update_user_tendencys(xp, prior_xp)
+  end
+
   def company_rank
     User.order(xp: :desc).index(self) + 1
   end
-
 
   private
 
   def give_anonyme_name
     self.first_name ||= Faker::Color.color_name
     self.last_name ||= Faker::Creature::Animal.name
+  end
+
+  def update_user_tendencys(new_xp, prior_xp)
+    lower_xp = new_xp - 1
+    if User.where(xp: (prior_xp..lower_xp)).blank?
+      self.tendency = 0
+    else
+      User.where(xp: (prior_xp..lower_xp)).each do |user|
+        user.tendency = -1
+        user.save
+      end
+      self.tendency = 1
+    end
+    save!
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_30_204653) do
+ActiveRecord::Schema.define(version: 2019_11_30_164415) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_name", null: false
@@ -23,9 +23,8 @@ ActiveRecord::Schema.define(version: 2019_11_30_204653) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer "prior_xp", default: 0
+    t.integer "tendency", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 end
