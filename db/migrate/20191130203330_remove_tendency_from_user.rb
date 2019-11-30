9bfa56bdeeb08771c7c8da656e753f305f339963
Luka Zdravkovic <luka.z@hotmail.de>
2019-11-30 22:31:28 +0100

    removes unlogical changes

--- db/migrate/20191130203330_remove_tendency_from_user.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTendencyFromUser < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :users, :tendency, :integer
-  end
-end
--- db/migrate/20191130204653_add_prior_xp_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddPriorXpToUser < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :prior_xp, :integer, default: 0
-  end
-end
