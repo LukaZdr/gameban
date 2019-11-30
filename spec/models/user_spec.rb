dfb1f6d1f464a8c5ffdeefc9a299ff37b3271fc4
Luka Zdravkovic <luka.z@hotmail.de>
2019-12-01 00:22:33 +0100

    adds rspec to the project

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'earning xp' do
+    it 'updates the users xp' do
+    context 'when passing another user' do
+      it 'makes the other users tendency negative'
+      it 'makes users tendency positive'
+    end
+    context 'when not passing anyone' do
+      it 'makes users tendency neutral'
+      it 'doesnt change other users tendency'
+    end
+  end
+end
