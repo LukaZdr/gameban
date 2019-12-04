06ad2c9d9fb3a6192e729a489126363188ecbb9f
Luka Zdravkovic <luka.z@hotmail.de>
2019-12-04 15:13:37 +0100

    test

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+
+{
+  externals: {
+    // require("jquery") is external and available
+    //  on the global var jQuery
+    jquery: "jQuery"
+  }
+}
