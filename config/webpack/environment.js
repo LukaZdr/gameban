f6ccdd94ab04077433b99e2e99b8684e66b9905e
Luka Zdravkovic <luka.z@hotmail.de>
2019-12-04 15:26:13 +0100

    moves js modules

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
-
-{
-  externals: {
-    // require("jquery") is external and available
-    //  on the global var jQuery
-    jquery: "jQuery"
-  }
-}
