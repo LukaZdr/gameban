6957ec652f0c6ebebce1887f5ddb1ac2db6b92e6
Luka Zdravkovic <luka.z@hotmail.de>
2019-12-28 11:39:29 +0100

    adds Ticket columns and places for them in project show

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,4 +2,6 @@ class Ticket < ApplicationRecord
   validates :name, :description, presence: true
   belongs_to :sprints
   belongs_to :user
+
+  COLUMNS = ['Backlog', 'Todo', 'In progress', 'Quality Assurance', 'Done']
 end
